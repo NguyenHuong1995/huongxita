59019ece3ac5984da6d78d5649ee4bf320fb85f2
nguyenhuong1995 <huongxita10@gmail.com>
2016-04-03 04:17:24 +0000

    coment by image

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -68,7 +68,7 @@ class CommentsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def comment_params
-      params.require(:comment).permit(:user_name, :body, :idea_id)
-    end
+   def comment_params
+   params.require(:comment).permit(:user_name, :body, :idea_id, :picture)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403031833) do
+ActiveRecord::Schema.define(version: 20160403033616) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20160403031833) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "like_count"
+    t.string   "picture"
+    t.integer  "reply_id"
   end
 
   create_table "ideas", force: :cascade do |t|
